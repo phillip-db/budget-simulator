2cda2906775e08f62f4501463f3d1e41ee62035e
Ananya Rajagopal <apr5@illinois.edu>
2023-11-02 18:02:12 -0500

    removed random top bar

--- BudgetSimulator/app/src/main/java/com/uiuc/budgetsimulator/MainActivity.java
+++ BudgetSimulator/app/src/main/java/com/uiuc/budgetsimulator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.uiuc.budgetsimulator;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
