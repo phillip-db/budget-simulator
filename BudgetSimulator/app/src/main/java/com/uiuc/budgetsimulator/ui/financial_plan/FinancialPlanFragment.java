e330113ad3d7fe7595fdfc3b437c4874301fe765
Ananya Rajagopal <apr5@illinois.edu>
2023-11-10 13:08:10 -0600

    income and fixed expenses done

--- BudgetSimulator/app/src/main/java/com/uiuc/budgetsimulator/ui/financial_plan/FinancialPlanFragment.java
+++ BudgetSimulator/app/src/main/java/com/uiuc/budgetsimulator/ui/financial_plan/FinancialPlanFragment.java
@@ -7,12 +7,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 import android.widget.EditText;
-import android.widget.TextView;
+
 import com.uiuc.budgetsimulator.R;
 
 
@@ -25,17 +23,17 @@ public class FinancialPlanFragment extends Fragment {
 
         // Find the views by their IDs
         TextView textFinancialPlan = root.findViewById(R.id.text_financial_plan);
-        EditText editText1 = root.findViewById(R.id.editText1);
-        EditText editText2 = root.findViewById(R.id.editText2);
-//        EditText editText3 = root.findViewById(R.id.editText3);
-//        EditText editText4 = root.findViewById(R.id.editText4);
+        EditText jobIncomeTextBox = root.findViewById(R.id.editText1);
+        EditText allowanceTextBox = root.findViewById(R.id.editText2);
+        EditText editText3 = root.findViewById(R.id.editText3);
+        EditText editText4 = root.findViewById(R.id.editText4);
 //        EditText editText5 = root.findViewById(R.id.editText5);
 
         // Set pre-filled values (if needed)
-        editText1.setText("123.45");
-        editText2.setText("678.90");
-//        editText3.setText("45.67");
-//        editText4.setText("890.12");
+        jobIncomeTextBox.setText("$150");
+        allowanceTextBox.setText("678.90");
+        editText3.setText("45.67");
+        editText4.setText("890.12");
 //        editText5.setText("123.45");
 
         return root;
