5a4df2f5e3032286dfe8fb70ffb83470d46f2a49
Phillip Burleigh <phillipbur01@gmail.com>
2023-11-16 20:34:03 -0600

    changes b4 going home

--- BudgetSimulator/app/src/main/java/com/uiuc/budgetsimulator/ui/reports/ReportsFragment.java
+++ BudgetSimulator/app/src/main/java/com/uiuc/budgetsimulator/ui/reports/ReportsFragment.java
@@ -18,67 +18,94 @@ import com.uiuc.budgetsimulator.MainActivity;
 import com.uiuc.budgetsimulator.R;
 import com.uiuc.budgetsimulator.Simulation;
 import com.uiuc.budgetsimulator.Utils;
+import com.uiuc.budgetsimulator.ui.financial_plan.FinancialPlanFragment;
 
 import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class ReportsFragment extends Fragment implements View.OnClickListener {
+public class ReportsFragment extends Fragment {
 
-  private RecyclerView recyclerView;
-  private ArrayList<ReportData> reportsList = new ArrayList<>();
-  public View onCreateView(@NonNull LayoutInflater inflater,
-                           ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_reports, container, false);
-  }
+    private RecyclerView recyclerView;
+    private ArrayList<ReportData> reportsList = new ArrayList<>();
+    private LinearLayout ll;
 
-  @Override
-  public void onViewCreated(View view,
-                            Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-
-    try {
-      InputStream is = getContext().openFileInput(Utils.REPORTS_SAVE_FILE);
-      ArrayList<Simulation> simulations = Utils.fromJSON(new TypeToken<ArrayList<Simulation>>() {
-      }.getType(), is);
-      is.close();
-      Simulation sim = Simulation.findSimByID(simulations, MainActivity.getGameSimId());
-
-      if (sim != null) reportsList = sim.getReports();
-    } catch (Exception e) {
-      e.printStackTrace();
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_reports, container, false);
     }
 
-    ReportsAdapter reportsAdapter = new ReportsAdapter(reportsList);
-    reportsAdapter.setOnClickListener((position, report) -> {
-      recyclerView.setVisibility(View.GONE);
-      LinearLayout ll = view.findViewById(R.id.report_summary);
-      ll.setVisibility(View.VISIBLE);
-    });
+    @Override
+    public void onViewCreated(View view,
+                              Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    recyclerView = view.findViewById(R.id.recyclerView);
-    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-    recyclerView.setAdapter(reportsAdapter);
+        try {
+            InputStream is = getContext().openFileInput(Utils.REPORTS_SAVE_FILE);
+            ArrayList<Simulation> simulations = Utils.fromJSON(new TypeToken<ArrayList<Simulation>>() {
+            }.getType(), is);
+            is.close();
+            Simulation sim = Simulation.findSimByID(simulations, MainActivity.getGameSimId());
 
-    TextView emptyView = view.findViewById(R.id.empty_view);
+            if (sim != null) reportsList = sim.getReports();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    if (reportsList.isEmpty()) {
-      recyclerView.setVisibility(View.GONE);
-      emptyView.setVisibility(View.VISIBLE);
-    } else {
-      recyclerView.setVisibility(View.VISIBLE);
-      emptyView.setVisibility(View.GONE);
+        LinearLayout ll = view.findViewById(R.id.report_summary);
+
+        ReportsAdapter reportsAdapter = new ReportsAdapter(reportsList);
+        reportsAdapter.setOnClickListener((position, report) -> {
+            recyclerView.setVisibility(View.GONE);
+            TextView title = view.findViewById(R.id.reports_page_title);
+            title.setText("Week #" + report.getWeekNumber());
+            ll.setVisibility(View.VISIBLE);
+
+            loadSummary(view, position, report);
+        });
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(reportsAdapter);
+
+        TextView emptyView = view.findViewById(R.id.empty_view);
+
+        MaterialButton cont = view.findViewById(R.id.report_continue);
+        cont.setOnClickListener(v -> {
+            ll.setVisibility(View.GONE);
+            TextView title = view.findViewById(R.id.reports_page_title);
+            title.setText(R.string.weekly_reports);
+            recyclerView.setVisibility(View.VISIBLE);
+        });
+
+        if (reportsList.isEmpty()) {
+            recyclerView.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        } else {
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+        }
     }
-  }
 
-  @Override
-  public void onClick(View v) {
-    switch (v.getId())
-    {
-      case R.id.weekText:
-        recyclerView.setVisibility(View.GONE);
-        LinearLayout ll = v.findViewById(R.id.report_summary);
-        ll.setVisibility(View.VISIBLE);
+    private void loadSummary(View view, int position, ReportData report) {
+        // Change spent/earned to be based on values in weekly report
+        ((TextView) view.findViewById(R.id.money_spent)).setText(String.format(Locale.ENGLISH, "$%d Spent", MainActivity.weekly_spending));
+        ((TextView) view.findViewById(R.id.money_earned)).setText(String.format(Locale.ENGLISH, "$%d Earned", MainActivity.weekly_earnings));
+        ((TextView) view.findViewById(R.id.total_savings))
+                .setText(
+                        String.format(Locale.ENGLISH,
+                                "Total Savings: $#->$%d",
+                                report.getMoney()));
+        ((TextView) view.findViewById(R.id.grade_change))
+                .setText(
+                        String.format(Locale.ENGLISH,
+                                "Grade: #->%d",
+                                report.getGrade()));
+        ((TextView) view.findViewById(R.id.health_change))
+                .setText(
+                        String.format(Locale.ENGLISH,
+                                "Health: #->%d",
+                                report.getHealth()));
     }
-  }
 }
